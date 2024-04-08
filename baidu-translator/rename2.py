94af8661809eba7b82edcbd34ceb8ae3097a73b5
artenax <107228652+artenax@users.noreply.github.com>
2024-04-09 04:29:51 +0700

    Create rename2.py

---
+++ baidu-translator/rename2.py
@@ -0,0 +1,11 @@
+import os
+
+def rename_files(folder_path):
+    files = os.listdir(folder_path)
+    files.sort(key=lambda x: int(''.join(filter(str.isdigit, x))))
+    for i, file_name in enumerate(files, start=1):
+        new_name = '{:04d}'.format(i) + '.mp3'
+        os.rename(os.path.join(folder_path, file_name), os.path.join(folder_path, new_name))
+
+folder_path = '/tmp/audio'
+rename_files(folder_path)
