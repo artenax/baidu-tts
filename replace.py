585657eedcaee14c593a8e47a54b4b709b09bc7d
artenax <107228652+artenax@users.noreply.github.com>
2024-06-07 01:01:31 +0700

    Update replace.py

--- replace.py
+++ replace.py
@@ -7,7 +7,7 @@ with open(filename, 'r', encoding='utf-8') as file:
 
 # Выполнение замены
 pattern = r'\n'
-replacement = '"\\nsleep 2 && wget --retry-on-http-error=503 --waitretry=10 --retry-connrefused -t 10 --user-agent=\"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36\" -O \"tts.mp3.$(date +%s).mp3\" \"https://fanyi.baidu.com/gettts?spd=3&source=web&lan=ru&text='
+replacement = '"\\nsleep 4 && wget --retry-on-http-error=503 --waitretry=10 --retry-connrefused -t 10 --user-agent=\"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36\" -O \"tts.mp3.$(date +%s).mp3\" \"https://fanyi.baidu.com/gettts?spd=3&source=web&lan=ru&text='
 modified_content = re.sub(pattern, replacement, content)
 
 # Запись измененного содержимого обратно в файл
